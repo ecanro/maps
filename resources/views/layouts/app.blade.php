b346b9d447b52d47cb70a80858031047792e6635
Jesus Avelar <jesus.avelar.nav@gmail.com>
2018-05-15 13:03:45 -0600

    ruta a la vista del CRUD de map

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -53,6 +53,7 @@
                             <li><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>
                             <li><a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a></li>
                         @else
+                            <li class="nav-item active"><a class="nav-link" href="{{route('maps.index')}}">Maps</a></li>
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
